ab0becc8bcf111815f11b393a9100980725ddc0a
Matthew Thorne <mwilsonthorne@gmail.com>
2018-04-02 14:29:11 +0100

    Added cargo objects to seed file

---
+++ db/seeds.rb
@@ -0,0 +1,18 @@
+require_relative('../models/transaction.rb')
+require_relative('../models/cargo.rb')
+
+
+cargo1 = Cargo.new({'description' => 'Space Food'})
+cargo2 = Cargo.new({'description' => 'Rocket Fuel'})
+cargo3 = Cargo.new({'description' => 'Ammunition'})
+cargo4 = Cargo.new({'description' => 'Spaceship Docking Tax'})
+cargo5 = Cargo.new({'description' => 'Gardulla'})
+
+
+
+
+# transaction1 = Transaction.new({'vendor_name' => 'Mos Eisley Cantina 2', 'planet_name' => 'Mars',
+# 'value' => 1000,
+#
+#
+#   })
