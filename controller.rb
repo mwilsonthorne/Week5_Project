75e173dc6cadf03dd44193e73765bef4a310a8f9
Matthew Thorne <mwilsonthorne@gmail.com>
2018-06-21 16:39:02 +0100

    Added Heroku

--- controller.rb
+++ controller.rb
@@ -1,5 +1,5 @@
 require('sinatra')
-require('sinatra/contrib/all')
+require('sinatra/contrib/all') if development?
 require_relative('models/transaction.rb')
 require_relative('models/cargo.rb')
 
--- db/sql_runner.rb
+++ db/sql_runner.rb
@@ -3,8 +3,12 @@ require('pg')
 class SqlRunner
 
   def self.run( sql, values = [])
-    db = PG.connect({dbname: 'smart_zenny',
-      host: 'localhost'  })
+    # db = PG.connect({dbname: 'smart_zenny',
+    #   host: 'localhost'  })
+    db = PG.connect( {dbname: 'Database',
+      host: 'ec2-1-1-1-1-1.compute-1.amazonaws.com',
+      port: 5432, user: 'mwilsonthorne@gmail.com', password: 'ZAQ!2wsx'
+      })
 
 
     db.prepare("save", sql)
@@ -17,3 +21,7 @@ class SqlRunner
 
 
 end
+
+# PG.connect( {dbname: 'dasdkdsaldkj',
+# host: 'ec2-1-1-1-1-1.compute-1.amazonaws.com',
+# port: 5432, user: 'nsdlkdjalskjd', password: 'sadlskjadlkjASDAD'})
